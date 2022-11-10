3c2881264c69c8899720e94efdd44a5facaf4f05
김준영 <ats166@naver.com>
2022-11-10 13:47:42 +0900

    update

--- new_pjt/src/components/VideoList.vue
+++ new_pjt/src/components/VideoList.vue
@@ -1,17 +1,31 @@
 <template>
   <div>
     <h1>VideoList</h1>
-    <VideoListItem/>
+    <VideoListItem
+      v-for="(video, index) in VideoList"
+      :key="`video-${index}`"
+      :video="video"
+    />
   </div>
 </template>
 
 <script>
 import VideoListItem from '@/components/VideoListItem'
+
 export default {
   name: 'VideoList',
+  created: {
+
+  },
+  computed: {
+    VideoList: function () {
+      return this.$store.state.VideoList
+    },
+  },
   components: {
-    VideoListItem
-  }
+    VideoListItem,
+  },
+
 }
 </script>
 
